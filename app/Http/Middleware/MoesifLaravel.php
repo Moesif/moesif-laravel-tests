44562f2464e7440af6e6c5b7e9a028a824710412
xinghengwang <xing@usetrove.com>
2016-10-24 17:49:58 -0700

    minor changes

--- app/Http/Middleware/MoesifLaravel.php
+++ app/Http/Middleware/MoesifLaravel.php
@@ -4,6 +4,8 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Support\Facades\Log;
+// use Aza\Components\Thread;
+
 
 class MoesifLaravel
 {
@@ -22,18 +24,33 @@ class MoesifLaravel
         Log::info('verb=' . $request->method());
         Log::info('url=' . $request->fullUrl());
         Log::info('ip=' . $request->ip());
-        // Log::info('header=' . $request->$headers.all());
+        Log::info('header=' . implode(', ', $request->headers->keys()));
         Log::info('user=' . $request->user());
         Log::info('session=' . $request->getSession());
 
         $response = $next($request);
 
-        // Log::info('res_headers=' . $response->$headers);
+        Log::info('res_headers=' . implode(', ', $response->headers->keys()));
         Log::info('res_body=' . $response->content());
         Log::info('res_status=' . $response->status());
 
         // do action after response
-        // Log::info('inside MoesifLaravel middleware after response=\n' . $response);
+
+
+        // $worker = new WorkerThread($request->headers);
+        // $thread = new WorkerThread();
+        // $thread->wait()->run($request->headers);
+
         return $response;
     }
 }
+
+// class WorkerThread extends Thread {
+//
+//     public function process() {
+//         $param = $this->getParam(0);
+//
+//         Log::info('inside thread: ');
+//     }
+//
+// }
